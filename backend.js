3dea7a1256cc3e644a7af6ed79139b0395e83e08
yagnaVNK <yagna.kaasaragadda@gmail.com>
2024-02-28 20:19:10 -0500

    added housing data to api

--- backend.js
+++ backend.js
@@ -39,6 +39,16 @@ app.get('/products', (req, res) => {
         res.json(JSON.parse(data));
     });
 });
+app.get('/housing', (req, res) => {
+    fs.readFile('./Data/housing.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal server error' });
+            return;
+        }
+        res.json(JSON.parse(data));
+    });
+});
 
 // Start the server
 app.listen(PORT, () => {
